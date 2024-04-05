3f164e68c6a5ebcf20b1827eb607d62fc8859f86
Havyas U <113080389+HavyasRai@users.noreply.github.com>
2024-04-05 20:43:14 +0530

    analytics commit

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -3,8 +3,9 @@ import Navbar from "../../components/navbar/Navbar";
 import Feeds from "../../components/Feeds/Feed";
 import { GlobalContext } from "../../context/context";
 import Loader from "../../components/cards/loader/loader";
-
+import { Analytics } from "@vercel/analytics/react"
 const Home = () => {
+
   const [category, setCategory] = useState("top Trending latest");
   const { searchInput, setSearchInput ,loading,
     setLoading} = useContext(GlobalContext);
@@ -17,6 +18,7 @@ const Home = () => {
       {loading && <Loader/>}
       <Navbar setCategory={setCategory} />
       <Feeds category={category} />
+      <Analytics/>
     </div>
   );
 };
